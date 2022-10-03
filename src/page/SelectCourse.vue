28175d673b9d5f33d882f19df1c374d71c03a01c
yule <305172302@qq.com>
2022-10-03 14:10:32 +0800

    rewrite the select course page with element-ui

--- src/page/SelectCourse.vue
+++ src/page/SelectCourse.vue
@@ -1,258 +1,238 @@
 <template>
-  <div class="layout">
-    <el-container>
-      <el-container>
-        <!-- 左侧 -->
-        <el-aside hide-trigger :style="{background: '#fff'}">
-          <!-- 学生信息 -->
-          <ul class="layout-info" style="list-style-type:none">
-            <li v-for="(item, index) of studentInfo" :key="index">{{item}}</li>
-          </ul>
-          <el-divider/>
-          <!-- 功能选择 -->
-          <el-menu :active-name="activeName" theme="light" width="auto" :open-names="['1']">
-            <el-submenu name="1" @click.native="select()">
-              <i class="el-icon-edit"></i>
-              <span>选课</span>
-            </el-submenu>
-            <el-submenu name="2" @click.native="showGrade()">
-              <i class="el-icon-b=view"></i>
-              <span>查看已修课程成绩</span>
-            </el-submenu>
-          </el-menu>
-          <el-divider/>
-          <div style="text-align: center">
-            <el-button type="primary" @click="handleLogOut()">退出</el-button>
+  <el-container>
+    <!-- header -->
+    <el-header>
+      <el-row>
+        <el-col :span="16">
+          <div style="font-size: 20px">
+            <b>Student Course Selection System</b>
           </div>
-        </el-aside>
-<!--        <Layout :style="{padding: '24px 24px 24px'}">-->
-<!--          <template v-if="isSelect">-->
-<!--            &lt;!&ndash; 上方搜索栏 &ndash;&gt;-->
-<!--            <Form ref="selectCourseInfo" :inline=true>-->
-<!--              <FormItem>-->
-<!--                <Input type="text" v-model="inputCNO" placeholder="请输入课程号"/>-->
-<!--              </FormItem>-->
-<!--              <FormItem>-->
-<!--                <Button type="primary" :disabled="!inputCNO" @click="handleSearch()">搜索</Button>-->
-<!--              </FormItem>-->
-<!--              <FormItem>-->
-<!--                <Button type="primary" :disabled="button1IsAble" @click="handleSelect()">选课</Button>-->
-<!--              </FormItem>-->
-<!--              <FormItem>-->
-<!--                <Button type="primary" :disabled="!inputCNO" @click="handleEmpty()">清空</Button>-->
-<!--              </FormItem>-->
-<!--            </Form>-->
-<!--            <div>-->
-<!--              &lt;!&ndash; 可选课程 &ndash;&gt;-->
-<!--              <Table border ref="selection" height=250 :columns="columnsCourse" :data="dataCourse" @on-selection-change="handleSelectChange"></Table>-->
-<!--            </div>-->
-<!--            <br>-->
-<!--            <div>-->
-<!--              <p style="display: inline">已选课程：</p>-->
-<!--              <Button style="display: inline" type="primary" :disabled="button2IsAble" @click="handleDrop()">退课</Button>-->
-<!--            </div>-->
-<!--            <br>-->
-<!--            <div>-->
-<!--              &lt;!&ndash; 已选课程 &ndash;&gt;-->
-<!--              <Table border ref="selection" height=250 :columns="columnsSelected" :data="dataSelected" @on-selection-change="handleDropChange"></Table>-->
-<!--            </div>-->
-<!--          </template>-->
-<!--          <template v-else>-->
-<!--            <div>-->
-<!--              <p>已修课程成绩：</p>-->
-<!--              <br>-->
-<!--              <Table border height=250 :columns="columnsGrade" :data="dataGrade"></Table>-->
-<!--            </div>-->
-<!--            <br>-->
-<!--            <Card>-->
-<!--              <div style="height: 590px">-->
-<!--                <h1 align='center'>学生成绩单</h1>-->
-<!--                <div align='right'>-->
-<!--                  <h2 style="display: inline">{{$route.params.userInfo.SNO}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$route.params.userInfo.SNAME}}</h2>-->
-<!--                  <h2 style="display: inline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{year}}/{{month}}/{{day}}</h2>-->
-<!--                </div>-->
-<!--                <Table border=false :columns="columnsShow" :data="dataShow"></Table>-->
-<!--                <br>-->
-<!--                <h2>平均成绩：{{avgGrade}}</h2>-->
-<!--              </div>-->
-<!--            </Card>-->
-<!--          </template>-->
-<!--        </Layout>-->
-      </el-container>
+        </el-col>
+        <el-col :span="8">
+          <template v-if="sNo==null">
+            <div style="text-align: right">
+              <el-button type="text" @click="handleSignOut()" style="color: #FFB200">Sign In</el-button>
+            </div>
+          </template>
+          <template v-else>
+            <div style="text-align: right">
+              {{ sNo }}&nbsp;&nbsp;{{ sName }}
+              <el-button type="text" @click="handleSignOut()" style="color: #FFB200">Sign Out</el-button>
+            </div>
+          </template>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container class="box">
+      <!-- aside -->
+      <el-aside>
+        <!-- function selection -->
+        <el-menu
+          :active-name="activeName"
+          default-active="1"
+          width="auto">
+          <el-menu-item index="1" @click.native="select()">
+            <i class="el-icon-edit"></i>
+            <span>Select Courses</span>
+          </el-menu-item>
+          <el-menu-item index="2" @click.native="showCompletedCourses()">
+            <i class="el-icon-document"></i>
+            <span>Check Completed Courses</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <template v-if="isSelect">
+        <!-- select course -->
+          <div>
+            <el-form ref="selectCourseInfo" :inline=true>
+              <el-form-item>
+                <el-input type="text" v-model="inputCourseCode" placeholder="Please enter the course code" style="width: 300px"/>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" :disabled="!inputCourseCode" @click="handleSearch()">Search</el-button>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" :disabled="button1IsAble" @click="handleSelect()">Select</el-button>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="info" :disabled="!inputCourseCode" @click="handleEmpty()">Clear</el-button>
+              </el-form-item>
+            </el-form>
+            <el-table
+              ref="multipleTable"
+              :data="dataCourse"
+              border
+              empty-text="No data."
+              @selection-change="handleSelectChange"
+              style="width: 100%">
+              <el-table-column type="selection" width="55"></el-table-column>
+              <el-table-column prop="cCode" label="Course Code"></el-table-column>
+              <el-table-column prop="cName" label="Course Name"></el-table-column>
+              <el-table-column prop="credit" label="Unit"></el-table-column>
+              <el-table-column prop="cDept" label="Department"></el-table-column>
+              <el-table-column prop="tName" label="Teacher"></el-table-column>
+            </el-table>
+          </div>
+          <br>
+          <div>
+            <el-row>
+              <el-col :span="16">
+                <div>
+                  <p>Selected Course(s): </p>
+                </div>
+              </el-col>
+              <el-col :span="8">
+                <div style="text-align: right">
+                  <el-button type="primary" :disabled="button2IsAble" @click="handleDrop()">Drop</el-button>
+                </div>
+              </el-col>
+            </el-row>
+            <el-table
+              ref="multipleTable"
+              :data="selectedCourses"
+              border
+              empty-text="No courses selected."
+              @selection-change="handleDropChange"
+              style="width: 100%">
+              <el-table-column type="selection" width="55"></el-table-column>
+              <el-table-column prop="cCode" label="Course Code"></el-table-column>
+              <el-table-column prop="cName" label="Course Name"></el-table-column>
+              <el-table-column prop="credit" label="Unit"></el-table-column>
+              <el-table-column prop="cDept" label="Department"></el-table-column>
+              <el-table-column prop="tName" label="Teacher"></el-table-column>
+            </el-table>
+          </div>
+        </template>
+        <template v-else>
+          <div>
+            <p>Completed Course(s):</p>
+          </div>
+          <el-table
+            :data="completedCourses"
+            border
+            empty-text="No courses completed."
+            style="width: 100%">
+            <el-table-column prop="cCode" label="Course Code"></el-table-column>
+            <el-table-column prop="cName" label="Course Name"></el-table-column>
+            <el-table-column prop="grade" label="Grade"></el-table-column>
+            <el-table-column prop="point" label="Point"></el-table-column>
+          </el-table>
+          <br>
+          <el-divider></el-divider>
+          <br>
+          <el-row>
+            <el-col :span="16">
+              <div>
+                <p>Transcript:</p>
+              </div>
+            </el-col>
+            <el-col :span="8">
+              <div style="text-align: right">
+                <el-button type="primary">Download</el-button>
+              </div>
+            </el-col>
+          </el-row>
+          <el-card>
+            <h1 align="center" style="font-size: 20px">Transcript</h1>
+            <div align="right">
+              <span>{{sNo}}&nbsp;&nbsp;{{sName}}&nbsp;&nbsp;</span>
+              <span>{{year}}.{{month}}.{{day}}</span>
+            </div>
+            <el-table
+              :data="completedCourses"
+              border
+              empty-text="No courses completed."
+              style="width: 100%">
+              <el-table-column prop="cCode" label="Course Code"></el-table-column>
+              <el-table-column prop="cName" label="Course Name"></el-table-column>
+              <el-table-column prop="grade" label="Grade"></el-table-column>
+              <el-table-column prop="point" label="Point"></el-table-column>
+              <el-table-column prop="credit" label="Unit"></el-table-column>
+            </el-table>
+            <p>GPA: {{avgGrade}}</p>
+          </el-card>
+        </template>
+      </el-main>
     </el-container>
-  </div>
+  </el-container>
 </template>
+
 <script>
 export default {
   data () {
     var date = new Date()
     return {
       // 学生信息
-      studentInfo: ['学号：' + this.$route.params.userInfo.SNO,
-        '姓名：' + this.$route.params.userInfo.SNAME,
-        '年龄：' + this.$route.params.userInfo.AGE,
-        '性别：' + this.$route.params.userInfo.SEX,
-        '所在系：' + this.$route.params.userInfo.SDEPT],
-      isSelect: true,					// 功能选择
-      activeName: '1',				// 初始功能
-      inputCNO: '',						// 搜索框
+      sNo: this.$route.params.no,       // student number
+      sName: this.$route.params.name,   // student name
+      isSelect: true,					          // function selection
+      activeName: '1',				          // default function
+      inputCourseCode: '',						  // search course input
       button1IsAble: true,
       button2IsAble: true,
-      // 可选课程
-      columnsCourse: [
-        {
-          type: 'selection',
-          width: 55,
-          align: 'center'
-        },
-        {
-          title: '课程号',
-          key: 'CNO'
-        },
-        {
-          title: '课程名',
-          key: 'CNAME'
-        },
-        {
-          title: '学分',
-          key: 'CREDIT'
-        },
-        {
-          title: '开课系',
-          key: 'CDEPT'
-        },
-        {
-          title: '开课教师',
-          key: 'TNAME'
-        }
-      ],
-      dataCourse: this.$route.params.courseInfo,
-      selectCNO: [],					// 待选课程
-      // 已选课程
-      columnsSelected: [
-        {
-          type: 'selection',
-          width: 55,
-          align: 'center'
-        },
-        {
-          title: '课程号',
-          key: 'CNO'
-        },
-        {
-          title: '课程名',
-          key: 'CNAME'
-        },
-        {
-          title: '学分',
-          key: 'CREDIT'
-        },
-        {
-          title: '开课系',
-          key: 'CDEPT'
-        },
-        {
-          title: '教师',
-          key: 'TNAME'
-        }
-      ],
-      dataSelected: this.$route.params.selectedInfo,
-      dropCNO: [],						// 待退课程
-      columnsGrade: [
-        {
-          title: '课程号',
-          key: 'CNO'
-        },
-        {
-          title: '课程名',
-          key: 'CNAME'
-        },
-        {
-          title: '成绩',
-          key: 'GRADE'
-        },
-        {
-          title: '绩点',
-          key: 'POINT'
-        }
-      ],
-      dataGrade: [],
+      dataCourse: '',                   // course data for table
+      selectedCourses: this.$route.params.selectedCourses,    // selected courses data for table
+      completedCourses: '',             // completed courses data for table
+      selectCourse: [],					        // course code for selecting
+      dropCourse: [],						        // course code for dropping
       year: date.getFullYear(),
       month: date.getMonth() + 1,
       day: date.getDate(),
-      columnsShow: [
-        {
-          title: '课程号',
-          align: 'center',
-          key: 'CNO'
-        },
-        {
-          title: '课程名',
-          align: 'center',
-          key: 'CNAME'
-        },
-        {
-          title: '成绩',
-          align: 'center',
-          key: 'GRADE'
-        },
-        {
-          title: '学分',
-          align: 'center',
-          key: 'CREDIT'
-        },
-        {
-          title: '教师',
-          align: 'center',
-          key: 'TNAME'
-        }
-      ],
-      dataShow: [],
       avgGrade: 0
     }
   },
   methods: {
+    handleSignOut() {
+      this.$router.push('/')
+    },
     select () {
       this.isSelect = true
     },
-    showGrade () {
-      this.$axios.post('http://localhost:5000/api/grade', {
-        SNO: this.$route.params.userInfo.SNO
+    showCompletedCourses () {
+      this.isSelect = false
+      this.$axios.get('http://localhost:5000/completedCourses', {
+        params: {
+          sNo: this.sNo
+        }
       }).then(response => {
-        this.dataGrade = response.data.gradeInfo
-        this.dataShow = response.data.showInfo
-        this.avgGrade = response.data.avgGrade
+        this.completedCourses = JSON.parse(response.data.completedCourses)
+        this.avgGrade = response.data.GPA
         console.log(response)
-        this.isSelect = false
       }).catch(error => {
         console.log(error)
         this.isSelect = false
       })
     },
     handleSelectChange (selection) {
-      // 选课状态改变
-      this.selectCNO = selection
-      if (selection.length === 0) {
-        this.button1IsAble = true
-      } else {
-        this.button1IsAble = false
-      }
+      // select courses selection change
+      this.selectCourse = selection
+      this.button1IsAble = selection.length === 0;
     },
     handleSelect () {
-      // 选课
-      this.$axios.post('http://localhost:5000/api/selectcourse', {
-        CNO: this.selectCNO,
-        SNO: this.$route.params.userInfo.SNO
+      // select courses
+      this.$axios.get('http://localhost:5000/selectCourse', {
+        params: {
+          course: JSON.stringify(this.selectCourse),
+          sNo: this.sNo
+        }
       }).then(response => {
         console.log(response)
-        if (response.data.fail.length === 0) {
-          this.$Message.success('选课成功！')
+        if (response.data.Status === "Success") {
+          this.$message({
+            message: 'Select course(s) successfully.',
+            type: 'success'
+          })
+        } else if (response.data.Status === "NotSignIn") {
+          this.$message.error('You do not sign in. Please sign in first.')
         } else {
-          var failCNO = response.data.fail[0]
-          for (var i = 1; i < response.data.fail.length; i++) {
-            failCNO = failCNO + '，' + response.data.fail[i]
+          var courses = JSON.parse(response.data.Courses)
+          console.log(courses)
+          var failCCode = courses
+          for (var i = 1; i < courses.length; i++) {
+            failCCode = failCCode + '，' + courses[i]
           }
-          this.$Message.error('已选择' + failCNO + '课程！')
+          this.$message.error('You have already selected course ' + failCCode + '.')
         }
         this.updateSelected()
       }).catch(error => {
@@ -260,32 +240,34 @@ export default {
       })
     },
     updateSelected () {
-      // 更新已选课程
-      this.$axios.post('http://localhost:5000/api/updateselected', {
-        SNO: this.$route.params.userInfo.SNO
+      // update selected courses
+      this.$axios.get('http://localhost:5000/updateSelected', {
+        params: {
+          sNo: this.sNo
+        }
       }).then(response => {
-        console.log(response)
-        this.dataSelected = response.data.selectedInfo
+        this.selectedCourses = JSON.parse(response.data.selectedCourses)
       }).catch(error => {
         console.log(error)
       })
     },
     handleDropChange (selection) {
-      // 退课状态改变
-      this.dropCNO = selection
-      if (selection.length === 0) {
-        this.button2IsAble = true
-      } else {
-        this.button2IsAble = false
-      }
+      // drop course selection change
+      this.dropCourse = selection
+      this.button2IsAble = selection.length === 0;
     },
     handleDrop () {
       // 退课
-      this.$axios.post('http://localhost:5000/api/dropcourse', {
-        CNO: this.dropCNO,
-        SNO: this.$route.params.userInfo.SNO
+      this.$axios.get('http://localhost:5000/dropCourse', {
+        params: {
+          dropCourse: JSON.stringify(this.dropCourse),
+          sNo: this.sNo
+        }
       }).then(response => {
-        this.$Message.success('退课成功！')
+        this.$message({
+          message: 'Drop the course(s) successfully.',
+          type: 'success'
+        })
         console.log(response)
         this.updateSelected()
       }).catch(error => {
@@ -294,52 +276,53 @@ export default {
     },
     handleSearch () {
       // 搜索课程
-      this.$axios.post('http://localhost:5000/api/searchcourse', {
-        CNO: this.inputCNO
+      this.$axios.get('http://localhost:5000/searchCourse', {
+        params: {
+          courseCode: this.inputCourseCode
+        }
       }).then(response => {
-        console.log(response)
-        this.dataCourse = response.data.courseInfo
+        this.dataCourse = response.data.dataCourse
       }).catch(error => {
         console.log(error)
       })
     },
     handleEmpty () {
       // 清空搜索框
-      this.inputCNO = ''
-      this.dataCourse = this.$route.params.courseInfo
+      this.inputCourseCode = ''
+      this.dataCourse = ''
     },
-    handleLogOut () {
-      this.$router.push({
-        name: 'App'
-      })
-    }
   }
 }
 </script>
+
 <style scoped>
-.layout{
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
+.el-header {
+  background-color: #277BC0;
+  color: #fff;
+  /*text-align: center;*/
+  line-height: 60px;
+  /*font-size: 20px;*/
 }
-.layout-logo{
-  width: 100px;
-  height: 30px;
-  background: #5b6270;
-  border-radius: 3px;
-  float: left;
-  position: relative;
-  top: 15px;
-  left: 20px;
+.el-aside {
+  background-color: #FFF4CF;
+  color: #333;
+  text-align: left;
+  line-height: 200px;
 }
-.layout-nav{
-  width: 420px;
-  margin: 0 auto;
-  margin-right: 20px;
+.el-main {
+  background-color: #fff;
+  color: #333;
+  /*text-align: center;*/
 }
-.layout-info{
-  margin: 20px;
+.el-menu {
+  background-color: #FFF4CF;
+  height: 100vh;
+}
+.el-container {
+  height: 100%
+}
+.el-menu-item.is-active {
+  background-color: #FFCB42;
+  color: #fff;
 }
 </style>
